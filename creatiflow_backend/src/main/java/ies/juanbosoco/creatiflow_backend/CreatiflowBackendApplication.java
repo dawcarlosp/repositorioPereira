eed1b79f519db28608ea4cb29354a44490ef2f20
dawcarlosp <dawcarlospereira@gmail.com>
2025-03-16 13:58:32 +0100

    Proyecto del back creado

---
+++ creatiflow_backend/src/main/java/ies/juanbosoco/creatiflow_backend/CreatiflowBackendApplication.java
@@ -0,0 +1,13 @@
+package ies.juanbosoco.creatiflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatiflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatiflowBackendApplication.class, args);
+	}
+
+}
---
+++ creatiflow_backend/src/test/java/ies/juanbosoco/creatiflow_backend/CreatiflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.juanbosoco.creatiflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatiflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
